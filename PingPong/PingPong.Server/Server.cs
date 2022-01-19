475911bfda2479f2a118249cdfdfff9b0f61597d
nivmamadov <62554360+nivmamadov@users.noreply.github.com>
2022-01-19 14:49:19 +0200

    Initialization of the server.

--- PingPong/PingPong.Server/Server.cs
+++ PingPong/PingPong.Server/Server.cs
@@ -6,6 +6,29 @@ namespace PingPong.Server
 {
     public class Server
     {
+        private Socket _listeningSocket;
+        private IPHostEntry _ipHostEntry;
+        private IPEndPoint _ipEndpoint;
+        private IPAddress _ipAddress;
 
+        private int _port;
+
+        public Server(int port)
+        {
+            _port = port;
+
+            _ipHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            _ipAddress = _ipHostEntry.AddressList[0];
+            _ipEndpoint = new IPEndPoint(_ipAddress, _port);
+            _listeningSocket = new Socket(_ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            InitialBinding();
+        }
+
+        public void InitialBinding()
+        {
+            _listeningSocket.Bind(_ipEndpoint);
+            _listeningSocket.Listen();
+        }
     }
 }
