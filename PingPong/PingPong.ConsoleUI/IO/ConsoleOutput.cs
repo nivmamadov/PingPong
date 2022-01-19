1021bdeaad323442d0df67541aa20cffe78d5563
nivmamadov <62554360+nivmamadov@users.noreply.github.com>
2022-01-19 14:59:05 +0200

    Added implementations of I/O abstractions. (Console I/O)

---
+++ PingPong/PingPong.ConsoleUI/IO/ConsoleOutput.cs
@@ -0,0 +1,17 @@
+﻿using PingPong.UI.IO.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.ConsoleUI.IO
+{
+    public class ConsoleOutput : IOutput<string>
+    {
+        public void SendOutput(string output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
