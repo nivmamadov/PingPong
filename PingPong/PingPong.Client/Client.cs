526a6369d9c70f1617d3904239ce67c87456cf17
nivmamadov <62554360+nivmamadov@users.noreply.github.com>
2022-01-19 14:43:10 +0200

    Initial Skeleton.

---
+++ PingPong/PingPong.Client/Client.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PingPong.Client
+{
+    public class Client
+    {
+    }
+}
---
+++ PingPong/PingPong.Server/Server.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+namespace PingPong.Server
+{
+    public class Server
+    {
+
+    }
+}
