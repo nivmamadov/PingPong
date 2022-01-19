e8fbcce3feae8fcf79e99e4a996b182c0382769f
nivmamadov <62554360+nivmamadov@users.noreply.github.com>
2022-01-19 15:45:34 +0200

    Client receiving & getting functionality added.

--- PingPong/PingPong.Client/Client.cs
+++ PingPong/PingPong.Client/Client.cs
@@ -30,6 +30,43 @@ namespace PingPong.Client
             _ipEndPoint = new IPEndPoint(_ipAddress, _port);
 
             _socket = new Socket(_ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                ConnectSocket();
+                SendMessageToServer("Test message.");
+            }
+            catch (Exception ex)
+            {
+                _output.SendOutput(ex.StackTrace);
+            }
+        }
+            
+        public void ConnectSocket()
+        {
+
+            _socket.Connect(_ipEndPoint);
+            _output.SendOutput($"Connection has been successfully made to {_socket.RemoteEndPoint}.");
+        }
+
+        public void SendMessageToServer(string message)
+        {
+            byte[] messageSent = Encoding.ASCII.GetBytes(message);
+            _socket.Send(messageSent);
+
+            GetMessageFromServer();
+
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        public void GetMessageFromServer()
+        {
+            byte[] messageReceived = new byte[1024];
+
+            int byteRecv = _socket.Receive(messageReceived);
+
+            Console.WriteLine($"Server has sent {Encoding.ASCII.GetString(messageReceived, 0, byteRecv)}.");
         }
     }
 }
