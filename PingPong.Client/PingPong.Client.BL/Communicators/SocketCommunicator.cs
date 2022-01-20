0b530af5fed2d108ff6ae6682bb1e10c06929462
nivmamadov <62554360+nivmamadov@users.noreply.github.com>
2022-01-20 09:24:54 +0200

    Added Socket communicator functionality.

---
+++ PingPong.Client/PingPong.Client.BL/Communicators/SocketCommunicator.cs
@@ -0,0 +1,33 @@
+﻿using PingPong.Client.BL.Communicators.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Client.BL.Communicators
+{
+    public class SocketCommunicator : ICommunicator
+    {
+        private Socket _clientSocket;
+
+        public SocketCommunicator(Socket clientSocket)
+        {
+            _clientSocket = clientSocket;
+        }
+
+        public byte[] Recieve()
+        {
+            byte[] messageReceived = new byte[1024];
+            _clientSocket.Receive(messageReceived);
+
+            return messageReceived;
+        }
+
+        public void Send(byte[] data)
+        {
+            _clientSocket.Send(data);
+        }
+    }
+}
